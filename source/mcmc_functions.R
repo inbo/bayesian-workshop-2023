54e4767a27cf4b6dd43b2c6d209be3410839bb7d
VAN DAELE <toon.vandaele@inbo.be>
2023-11-12 15:58:37 +0100

    change a,b to beta0 and beta1

--- source/mcmc_functions.R
+++ source/mcmc_functions.R
@@ -1,10 +1,10 @@
-# Likelihood function
+# Likelihood function for the data (log)
 ll.fun <- function(a, b, tau = 0.228){
   ll <- sum(dnorm(x = y, mean = a + b * x, sd = sqrt(1 / tau),log = TRUE))
   return(ll)
 }
 
-# prior function (uninformative)
+# prior function (uninformative) (log)
 prior.fun <- function(a, b, tau = 0.228){
   lp <- dnorm(x = a, mean = 0, sd = sqrt(1/10^-6), log = TRUE) +
     dnorm(x = b, mean = 0, sd = sqrt(1/10^-6), log = TRUE) +
@@ -16,45 +16,45 @@ prior.fun <- function(a, b, tau = 0.228){
 
 # N = lengte van de MCMC
 # sigma = grootte van de stappen in de markov chain
-# init_a en init_b = initiële waarden voor a en b
+# init_b0 en init_b1 = initiële waarden voor beta_0 en beta_1
 
-MCMC_metro <- function(x, y, N = 20, sigma = 1, init_a, init_b){
+MCMC_metro <- function(x, y, N = 20, sigma = 1, init_b0, init_b1){
 
-  set.seed(56)
+  set.seed(56)  # Optioneel, om bij iedereen hetzelfde resultaat te bekomen.
   # We maken een lege dataframe om de output in te bewaren
-  mcmc <- data.frame(iter = 1:N,
-                     a = NA,
-                     b = NA,
-                     tau = NA,
-                     posterior = NA,
-                     accept = NA)
+  mcmc <- data.frame(iter = 1:N,     # positie in de markov chain
+                     beta_0 = NA,    # intercept
+                     beta_1 = NA,    # helling
+                     tau = NA,       # 1/standaard afwijking
+                     post = NA, # posterior
+                     accept = NA)    # stap wel (1) of niet (0) geaccepteerd.
 
-  mcmc$a[1] <- init_a
-  mcmc$b[1] <- init_b
-  mcmc$tau[1] <- 0.228  # init_tau
+  mcmc$beta_0[1] <- init_b0
+  mcmc$beta_1[1] <- init_b1
+  mcmc$tau[1] <- 0.228  # init_tau voor de eenvoud houden we tau fixed.
 
-  # Berekening van de posterior voor de eerste lijn (met de startwaarde)
-  mcmc$posterior[1] <- ll.fun(a = mcmc$a[1], b = mcmc$b[1], tau = mcmc$tau[1]) +
-    prior.fun(a = mcmc$a[1], b = mcmc$b[1], tau = mcmc$tau[1])
-  mcmc$accept[1] <- 1   # De initiële waarde wordt altijd geaccepteerd
+  # Berekening van de posterior voor de startwaarde
+  mcmc$post[1] <- ll.fun(a = mcmc$beta_0[1], b = mcmc$beta_1[1], tau = mcmc$tau[1]) +
+    prior.fun(a = mcmc$beta_0[1], b = mcmc$beta_1[1], tau = mcmc$tau[1])
+  mcmc$accept[1] <- 1  # De initiële parameterwaarde altijd accepteren
 
   for(i in 2:N){
 
-    # Kies een nieuwe waarde uit een normale verdeling
-    # met gemiddelde = vorige waarde en standaard afwijking = sigma (stapgrootte)
-    new_a <- rnorm(1, mean = mcmc$a[i - 1], sd = sigma)
-    new_b <- rnorm(1, mean = mcmc$b[i - 1], sd = sigma)
+    # Kies een nieuwe waarde uit een normale verdeling met
+    # mean = vorige parameterwaarde en sd = sigma (stapgrootte)
+    new_beta_0 <- rnorm(1, mean = mcmc$beta_0[i - 1], sd = sigma)
+    new_beta_1 <- rnorm(1, mean = mcmc$beta_1[i - 1], sd = sigma)
     #new_tau <- rnorm(1, mean = mcmc$tau[i - 1], sd = sigma)
-    new_tau <- mcmc$tau[i -1]
+    new_tau <- mcmc$tau[i -1]  #we houden tau fixed voor de eenvoud
 
     # Bereken de posterior
-    new_post <- ll.fun(a = new_a, b = new_b, tau = new_tau) +
-      prior.fun(a = new_a, b = new_b, tau = new_tau)
+    new_post <- ll.fun(a = new_beta_0, b = new_beta_1, tau = new_tau) +
+      prior.fun(a = new_beta_0, b = new_beta_1, tau = new_tau)
 
-    # Bereken de log ratio  (in de log schaal is dit het verschil)
+    # Bereken de ratio van de posterios (in de log schaal is dit het verschil)
     log.ratio <- new_post - mcmc$post[i-1]
 
-    # Als de log.ratio > 0 => accepteren
+    # Als de ratio > 0 => nieuwe posterior beter = > accepteren
     if (log.ratio > 0) {
       mcmc$accept[i] <- 1
       # Als log.ratio < 0 is de kans op accepteren afhankelijk van de log.ratio
@@ -68,16 +68,16 @@ MCMC_metro <- function(x, y, N = 20, sigma = 1, init_a, init_b){
 
     # De nieuwe waarde wordt geaccepteerd
     if(mcmc$accept[i]) {
-      mcmc$a[i] <- new_a
-      mcmc$b[i] <- new_b
+      mcmc$beta_0[i] <- new_beta_0
+      mcmc$beta_1[i] <- new_beta_1
       mcmc$tau[i] <- new_tau
-      mcmc$posterior[i] <- new_post
+      mcmc$post[i] <- new_post
     }else{
-      # De nieuwe waarde wordt niet geaccepteerd (de oude blijft behouden)
-      mcmc$a[i] <- mcmc$a[i-1]
-      mcmc$b[i] <- mcmc$b[i-1]
+      # De nieuwe waarde wordt niet geaccepteerd (de oude behouden)
+      mcmc$beta_0[i] <- mcmc$beta_0[i-1]
+      mcmc$beta_1[i] <- mcmc$beta_1[i-1]
       mcmc$tau[i] <- mcmc$tau[i-1]
-      mcmc$posterior[i] <- mcmc$posterior[i-1]
+      mcmc$post[i] <- mcmc$post[i-1]
     }
   }
   return(mcmc = mcmc)
